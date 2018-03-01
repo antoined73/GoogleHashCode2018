601785e2b12708e892861a8df16a530d7380cfae
Lucas <lucas.matteo@etu.unice.fr>
2018-03-01 20:35:00 +0100

    un max de points

--- src/TuLeC/process/SimpleProcess.java
+++ src/TuLeC/process/SimpleProcess.java
@@ -3,6 +3,7 @@ package TuLeC.process;
 import TuLeC.Obj.Car;
 import TuLeC.Obj.Ride;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class SimpleProcess {
@@ -16,6 +17,19 @@ public class SimpleProcess {
 
     public void process(){
         int i = 0;
+
+        rides.sort(new Comparator<Ride>() {
+            public int compare(Ride o1, Ride o2) {
+                if (o1.earliestStart > o2.earliestStart){
+                    return 1;
+                } else if (o1.earliestStart == o2.earliestStart){
+                    return 0;
+                } else {
+                    return -1;
+                }
+            }
+        });
+
         for (Ride ride : rides){
             cars.get(i++%cars.size()).addRide(ride);
         }
