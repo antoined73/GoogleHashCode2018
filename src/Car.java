737c5b2584f1ee005629cb3eb38d1407db022328
Jehan <jehanmilleret@gmail.com>
2018-03-01 19:24:53 +0100

    Intersection + car

---
+++ src/Car.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+    public Intersection position;
+    public List<Ride> rides;
+
+    public Car(){
+        rides = new ArrayList<Ride>();
+        position = new Intersection(0, 0);
+    }
+
+    public void moveTo(Intersection intersection){
+        this.position = intersection;
+    }
+
+    public void addRide(Ride ride){
+        rides.add(ride);
+    }
+
+    @Override
+    public String toString(){
+        if(rides.size() == 0) return "0";
+        StringBuilder res = new StringBuilder(rides.size() + " ");
+        for (Ride ride: rides) {
+            res.append(ride.toString()).append(" ");
+        }
+        return res.toString();
+    }
+}
--- src/Ride.java
+++ src/Ride.java
@@ -1,14 +1,14 @@
 import java.awt.*;
 
 public class Ride {
-    Point startPoint;
-    Point endPoint;
+    Intersection startPoint;
+    Intersection endPoint;
     int earliestStart;
     int latestFinish;
 
     public Ride(int srow, int scolumn, int frow, int fcolumn, int earliestStart, int latestFinish) {
-        this.startPoint = new Point(srow,scolumn);
-        this.endPoint = new Point(frow,fcolumn);
+        this.startPoint = new Intersection(srow,scolumn);
+        this.endPoint = new Intersection(frow,fcolumn);
         this.earliestStart = earliestStart;
         this.latestFinish = latestFinish;
     }
