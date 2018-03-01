9246a5a1dcb5c138b591dc61276eabe5bf0c5c1a
Lucas <lucas.matteo@etu.unice.fr>
2018-03-01 19:36:10 +0100

    process

---
+++ src/process/SimpleProcess.java
@@ -0,0 +1,12 @@
+package process;
+
+import java.util.List;
+
+public class SimpleProcess {
+    private List<Car> cars;
+    private List<Ride> rides;
+
+    public SimpleProcess(List<Car> cars, List<Ride> rides){
+
+    }
+}
